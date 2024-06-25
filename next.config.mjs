299b042337ec2e25bf1d3567772b4b0faeb41046
Rizel Scarlett <rizel@BLKMVXJJXDYVV.local>
2024-06-25 06:24:06 -0400

    Landing page and example web5

---
+++ next.config.mjs
@@ -0,0 +1,33 @@
+import webpack from 'webpack';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        stream: 'stream-browserify',
+        crypto: 'crypto-browserify',
+        buffer: 'buffer',
+      };
+
+      config.plugins.push(
+        new webpack.ProvidePlugin({
+          process: 'process/browser',
+          Buffer: ['buffer', 'Buffer'],
+        }),
+        new webpack.NormalModuleReplacementPlugin(
+          /node:crypto/,
+          (resource) => {
+            resource.request = resource.request.replace(/^node:/, '');
+          }
+        )
+      );
+    }
+    return config;
+  },
+};
+
+export default nextConfig;
