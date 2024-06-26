06b10a69575e7205f7b7a7754952cdd58e90d3c2
Angela Hwang <hwangangela99@hotmail.com>
2024-06-26 13:55:17 -0700

    Dialog to add end date (#8)

---
+++ src/components/modal.jsx
@@ -0,0 +1,35 @@
+
+import React, { useState } from 'react';
+
+
+export function Modal({children}) {
+  return (
+    <dialog id="my_modal_1" className="modal">
+      {children}
+    </dialog>
+  )
+}
+
+
+export const DynamicDialog = ({title, dialogContent, onClose}) => {
+
+  return (
+    <div className="p-4">
+        <div className="modal modal-open">
+          <div className="modal-box">
+            <h3 className="font-bold text-lg">{title}</h3>
+            <p className="py-4">{dialogContent}</p>
+            <div className="modal-action">
+              <button
+                className="btn"
+                onClick={onClose}
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+    </div>
+  );
+};
+
---
+++ src/components/period-dialog.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { DynamicDialog } from "@/components/modal"
+
+const formatDate = (date) => {
+  if (!date) return '';
+  let d = new Date(date),
+      month = '' + (d.getMonth() + 1),
+      day = '' + d.getDate(),
+      year = d.getFullYear();
+
+  if (month.length < 2) 
+      month = '0' + month;
+  if (day.length < 2) 
+      day = '0' + day;
+
+  return [year, month, day].join('-');
+};
+
+export const PeriodDialog = ({ dialogTitle, event, onClose }) => {
+  const [localStartDate, setLocalStartDate] = useState(formatDate(event.start));
+  const [localEndDate, setLocalEndDate] = useState(formatDate(event.end));
+
+  const handleStartDateChange = (e) => {
+    setLocalStartDate(e.target.value);
+  };
+  const handleEndDateChange = (e) => {
+    setLocalEndDate(e.target.value);
+  };
+
+  const handleClose = () => {
+    onClose(localStartDate, localEndDate);
+  };
+
+  const dialogContent = (
+      <div>
+        <label htmlFor="start">Started: </label>
+        <input type="date" id="start" name="start" value={localStartDate} onChange={handleStartDateChange} />
+        <br/>
+        <label htmlFor="end">Ended: </label>
+        <input type="date" id="end" name="end" value={localEndDate} onChange={handleEndDateChange} />
+      </div>
+    );
+  
+  return (<DynamicDialog title={dialogTitle} dialogContent={dialogContent} onClose={handleClose} />);
+}
