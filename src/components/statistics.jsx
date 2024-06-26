4e207dd0d42c20c1b2e32949013667ca6354ef75
Natasha Alexandrou <natashaa@squareup.com>
2024-06-25 23:09:52 -0700

    Add basic statistics component at bottom of landing page

---
+++ src/components/statistics.jsx
@@ -0,0 +1,19 @@
+
+export function Statistics() {
+
+    return (
+        <div>
+
+        <h3>Your average cycle length is</h3>
+            <div>28 days</div>
+
+        <h3>Your next predicted cycle will start on</h3>
+            <div>Fri, 5 July 2024</div>
+
+        <h3>First day of last period</h3>
+            <div>Fri, 7 June 2024</div>
+
+        </div>
+    );
+
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { LandingPage } from "@/components/landingpage";
+import { Statistics } from "@/components/statistics";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <main>
       <LandingPage />
+      <Statistics />
     </main>
   );
 }
