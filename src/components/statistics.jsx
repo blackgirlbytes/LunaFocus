24ed08007dc1360f4d1b89f20cddf01dfc895438
Natasha Alexandrou <natashaa@squareup.com>
2024-06-26 02:56:28 -0700

    Add styling using Tailwind

--- src/components/statistics.jsx
+++ src/components/statistics.jsx
@@ -1,19 +1,28 @@
-
 export function Statistics() {
 
     return (
-        <div>
+      <div className="container mx-auto p-4">
 
-        <h3>Your average cycle length is</h3>
-            <div>28 days</div>
-
-        <h3>Your next predicted cycle will start on</h3>
-            <div>Fri, 5 July 2024</div>
-
-        <h3>First day of last period</h3>
-            <div>Fri, 7 June 2024</div>
-
+        <div className="space-y-2">
+          <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none text-[#000080]">
+            Statistics
+          </h1>
+          <p className="max-w-[600px] text-[#333333] md:text-xl">
+            These statistics have been calculate based on your last three cycles.
+          </p>
         </div>
+  
+        <main className="flex flex-col items-center justify-center py-4">
+          <h2 className="text-xl font-bold mb-4">Your average cycle length is</h2>
+          <p className="text-xl mb-8 text-center">28 days</p>
+  
+          <h2 className="text-xl font-bold mb-4">Your next predicted cycle will start on</h2>
+          <p className="text-xl mb-8 text-center">Fri, 5 July 2024</p>
+  
+          <h2 className="text-xl font-bold mb-4">First day of last period</h2>
+          <p className="text-xl mb-8 text-center">Fri, 7 June 2024</p>
+        </main>
+      </div>
     );
 
 }
