c856f96bc921456318b7d5197c9c91c530fe0452
Natasha Alexandrou <natashaa@squareup.com>
2024-06-26 05:15:17 -0700

    Remove unused component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { LandingPage } from "@/components/landingpage";
-import { Statistics } from "@/components/statistics";
 
 const inter = Inter({ subsets: ["latin"] });
 
