7eb15439fb0d9702a4c007ae0abee55ce2869bce
blackgirlbytes <blackgirlbytes@github.com>
2024-06-26 22:04:01 -0400

    adding stats component with dummy data

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -1,8 +1,12 @@
 import { CalendarPage } from "@/components/calendarpage";
+import { StatsPage } from "@/components/statspage";
 
 export default function Calendar() {
   return (
     <main>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <StatsPage />
+      </div>
       <CalendarPage />
     </main>
   );
