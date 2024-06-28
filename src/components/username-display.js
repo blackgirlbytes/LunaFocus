d5fd8668ac5b4944835a6e8c02f64278918571a4
James Nettesheim <jnettesheim@squareup.com>
2024-06-28 12:24:06 -0500

    Final changes

--- src/components/username-display.js
+++ src/components/username-display.js
@@ -6,14 +6,21 @@ export function UsernameDisplay() {
     return (
         username ? (
             <div className="p-5">
-                <h1 className="text-5xl font-bold">Hi, {username} 👋 </h1>
+                <h1 className="text-5xl font-bold">
+                    <Link
+                            className="text-5xl font-bold hover:text-[#000080] text-[#333333]"
+                            href="/Login"
+                            prefetch={false}>
+                            Hi, {username} 👋 
+                    </Link>
+                </h1>
             </div>
         ) : (!isLoading &&
             <div className="p-5">
                 <h1 className="text-5xl font-bold">
                     <Link
                         href="/Login"
-                        className="text-sm font-medium hover:text-[#000080] text-[#333333]"
+                        className="text--5xl font-bold hover:text-[#000080] text-[#333333]"
                         prefetch={false}>
                         Click here to login
                     </Link>
